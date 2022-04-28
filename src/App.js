05664fff166cc7200ac73fbf1de08d71f3d81f83
KaterynaKolomiiets <kateryna.kolomiiets.official@gmail.com>
2022-04-28 13:53:42 +0200

    added styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import "./App.css";
-
 import { useState } from "react";
 
 function App() {
@@ -14,13 +12,18 @@ function App() {
   return (
     <div className="App">
       <div className="counter">
-        <button type="button" onClick={increment}>
+        <button
+          className="btn"
+          type="button"
+          name="increment"
+          onClick={increment}
+        >
           Increment
         </button>
         <div>
-          <span>Counter:</span> <span> {value}</span>
+          <span>Counter:</span> <span className={value===0 && "zero"}> {value}</span>
         </div>
-        <button type="button" onClick={reset}>
+        <button className="btn" type="button" name="reset" onClick={reset}>
           Reset
         </button>
       </div>
@@ -29,17 +32,3 @@ function App() {
 }
 
 export default App;
-
-// Create a new public repository on your github account to host the source code of a react app.
-// On your local machine and using a language of your choice, create a react app (let's call it "counter") with following elements:
-// - A button with the label: "Increment"
-// - A button with the label: "Reset"
-// - A number prefixed by the label "Counter:"
-
-// Behavior: Once the react page is loaded the number should be set to 0(zero).
-// With every push on the "Increment" button this number increments by 1.  Pushing the "Reset" counter resets the number to zero.
-
-// The page should include a stylesheet that applies two different(of your choice) colors to the buttons
-// when in the normal and when in the pressed state.Also the number should show a different color when it is 0.
-
-// The github repo should show at least 2 commits instead of just a single commit where everything got uploaded.
